eff58401849af3076954b6f1f77604eac8bb23cf
Athersh-SR <athersh946@gmail.com>
2025-06-06 14:04:00 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    $('.nav-links .nav-item a').on('click', function () {
+      $('.nav-links .nav-item').removeClass('active');
+      $(this).parent().addClass('active');
+    });
+  });
+  function animateCounter(counter) {
+    const target = +counter.getAttribute("data-target");
+    let current = 0;
+    const speed = 200;
+    const increment = target / speed;
+
+    function update() {
+      current += increment;
+      if (current < target) {
+        counter.innerText = Math.ceil(current);
+        requestAnimationFrame(update);
+      } else {
+        counter.innerText = target;
+      }
+    }
+
+    update();
+  }
+
+  const counters = document.querySelectorAll(".stat");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          animateCounter(entry.target);
+        } else {
+          entry.target.innerText = "0";
+        }
+      });
+    },
+    {
+      threshold: 0.5,
+    }
+  );
+
+  counters.forEach((counter) => {
+    observer.observe(counter);
+  });
+
