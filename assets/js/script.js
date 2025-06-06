453500b5f6545cd2f46bd5321b29d1564e155651
Athersh-SR <athersh946@gmail.com>
2025-06-06 14:33:22 +0530

    third commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,41 @@
-$(document).ready(function () {
-    $('.nav-links .nav-item a').on('click', function () {
-      $('.nav-links .nav-item').removeClass('active');
-      $(this).parent().addClass('active');
-    });
-  });
-  function animateCounter(counter) {
-    const target = +counter.getAttribute("data-target");
-    let current = 0;
-    const speed = 200;
-    const increment = target / speed;
 
-    function update() {
-      current += increment;
-      if (current < target) {
-        counter.innerText = Math.ceil(current);
-        requestAnimationFrame(update);
-      } else {
-        counter.innerText = target;
-      }
+function animateCounter(counter) {
+  const target = +counter.getAttribute("data-target");
+  let current = 0;
+  const speed = 200;
+  const increment = target / speed;
+
+  function update() {
+    current += increment;
+    if (current < target) {
+      counter.innerText = Math.ceil(current);
+      requestAnimationFrame(update);
+    } else {
+      counter.innerText = target;
     }
-
-    update();
   }
 
-  const counters = document.querySelectorAll(".stat");
+  update();
+}
 
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          animateCounter(entry.target);
-        } else {
-          entry.target.innerText = "0";
-        }
-      });
-    },
-    {
-      threshold: 0.5,
-    }
-  );
+const counters = document.querySelectorAll(".stat");
 
-  counters.forEach((counter) => {
-    observer.observe(counter);
-  });
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        animateCounter(entry.target);
+      } else {
+        entry.target.innerText = "0";
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+counters.forEach((counter) => {
+  observer.observe(counter);
+});
 
